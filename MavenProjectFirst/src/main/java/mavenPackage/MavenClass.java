7bc4713a181250d5d48522bb7a26c6993f0d8f0b
devika <devikamurali08@gmail.com>
2025-01-01 11:26:20 +0530

    My first committed project in obsqura

---
+++ MavenProjectFirst/src/main/java/mavenPackage/MavenClass.java
@@ -0,0 +1,15 @@
+package mavenPackage;
+
+public class MavenClass {
+
+	public static void main(String[] args) {
+		int i;
+		int sum = 0;
+		for (i = 1; i <= 10; i++) {
+			sum = sum + i;
+		}
+		System.out.println("sum : "+sum);
+
+	}
+
+}
